076a039ac837c62fc4d221bf7679a331c94713cb
Jym-lab <jym9809@gmail.com>
2023-08-22 10:13:35 +0900

    fix: add username field

--- models/users.py
+++ models/users.py
@@ -1,12 +1,12 @@
 from typing import List, Optional
 from pydantic import BaseModel, EmailStr
-
 from models.events import Event
 
 # 사용자 모델
 class User(BaseModel):
 	email: EmailStr
 	password: str
+	username: str
 	events: Optional[List[Event]]
 	# 서브 클래스 Config는 API를 문서화할 때 샘플 데이터를 보여주기 위함이다.
 	class Config:
